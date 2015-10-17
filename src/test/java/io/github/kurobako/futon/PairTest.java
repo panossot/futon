053f02b09cf92e80a93319755819d9f28af8f850
wyyyrd <wyyyrd@gmail.com>
2015-10-17 23:41:22 +0200

    tests for pair(null, null)

--- src/test/java/io/github/kurobako/futon/PairTest.java
+++ src/test/java/io/github/kurobako/futon/PairTest.java
@@ -18,45 +18,46 @@
 
 package io.github.kurobako.futon;
 
-import org.junit.Rule;
-import org.junit.experimental.theories.DataPoint;
-import org.junit.experimental.theories.Theories;
-import org.junit.experimental.theories.Theory;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
 import static io.github.kurobako.futon.Pair.pair;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
-@RunWith(Theories.class)
 public class PairTest {
 
-  @Theory
-  public void testSwap(final Pair<String, String> mock) {
-    Pair<String, String> swapped = mock.swap();
+  @Test
+  public void testSwap() {
+    Pair<String, String> swapped = pair("a", "b").swap();
     assertEquals(swapped.left, "b");
     assertEquals(swapped.right, "a");
   }
 
-  @Theory
-  public void testHashCode(final Pair<String, String> mock) {
-    assertEquals(mock.hashCode(), pair("a", "b").hashCode());
+  @Test
+  public void testHashCode() {
+    Pair<String, String> first = pair("a", "b");
+    Pair<String, String> second = pair("a", "b");
+    assertEquals(first.hashCode(), second.hashCode());
   }
 
-  @Theory
-  public void testEquals(final Pair<String, String> mock) {
-    assertEquals(mock, pair("a", "b"));
-    assertNotEquals(mock, new Object());
-    assertNotEquals(mock, pair("a", "a"));
-    assertNotEquals(mock, pair("b", "b"));
-    assertNotEquals(mock, null);
+  @Test
+  public void testHashCodeNulls() {
+    Pair<String, String> nulls = pair(null, null);
+    //noinspection ResultOfMethodCallIgnored
+    nulls.hashCode();
   }
 
+  @Test
+  public void testEquals() {
+    assertEquals(pair("a", "b"), pair("a", "b"));
+    assertNotEquals(pair("a", "b"), pair("a", new Object()));
+    assertNotEquals(pair("a", "b"), pair(new Object(), "b"));
+    assertNotEquals(pair("a", "b"), new Object());
+    assertNotEquals(pair("a", "b"), null);
+  }
 
-  @Rule
-  public ExpectedException thrown = ExpectedException.none();
-
-  @DataPoint
-  public static final Pair<String, String> ab = pair("a", "b");
+  @Test
+  public void testEqualsNulls() {
+    assertEquals(pair(null, null), pair(null, null));
+  }
 }
