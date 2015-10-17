50ecd9a3b282eb3462567dacadcfb934e5aa2fb0
wyyyrd <wyyyrd@gmail.com>
2015-10-18 00:11:48 +0200

    Predicate

---
+++ src/main/java/io/github/kurobako/futon/Predicate.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2015 Fedor Gavrilov
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+package io.github.kurobako.futon;
+
+import javax.annotation.Nonnull;
+
+import static java.util.Objects.requireNonNull;
+
+@FunctionalInterface
+public interface Predicate<A> extends Function<A, Boolean> {
+
+  default @Nonnull Predicate<A> not() {
+    return a -> !$(a);
+  }
+
+  default @Nonnull Predicate<A> and(final @Nonnull Predicate<? super A> predicate) {
+    requireNonNull(predicate, "predicate");
+    return a -> this.$(a) && predicate.$(a);
+  }
+
+  default @Nonnull Predicate<A> or(final @Nonnull Predicate<? super A> predicate) {
+    requireNonNull(predicate, "predicate");
+    return a -> this.$(a) || predicate.$(a);
+  }
+
+  default @Nonnull Predicate<A> xor(final @Nonnull Predicate<? super A> predicate) {
+    requireNonNull(predicate, "predicate");
+    return a -> this.$(a) ^ predicate.$(a);
+  }
+
+  static @Nonnull <A> Predicate<A> TRUE() {
+    return a -> Boolean.TRUE;
+  }
+
+  static @Nonnull <A> Predicate<A> FALSE() {
+    return a -> Boolean.FALSE;
+  }
+}
