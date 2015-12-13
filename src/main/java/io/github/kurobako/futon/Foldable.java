5000cfaa1707ebc0f1b10ffb43f45def75a8ad32
wyyyrd <wyyyrd@gmail.com>
2015-12-13 16:25:27 +0100

    Foldable.fold default implementation now uses left fold

--- src/main/java/io/github/kurobako/futon/Foldable.java
+++ src/main/java/io/github/kurobako/futon/Foldable.java
@@ -27,7 +27,7 @@ public interface Foldable<A> {
   <M extends Semigroup<M>> M fold(final @Nonnull Function<? super A, ? extends M> function, final @Nonnull M empty) {
     requireNonNull(function, "function");
     requireNonNull(empty, "empty");
-    return foldRight((a, m) -> m.append(function.$(a)), empty);
+    return foldLeft((m, a) -> m.append(function.$(a)), empty);
   }
 
   <B> B foldRight(@Nonnull BiFunction<? super A, ? super B, ? extends B> function, B initial);
