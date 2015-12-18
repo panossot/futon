4046eda5c4f3c9d65ed05117b0ae2d303b15d7f4
wyyyrd <wyyyrd@gmail.com>
2015-12-18 14:58:02 +0100

    test case: ComonadLaws now uses junit theories

--- src/test/java/io/github/kurobako/futon/ComonadLaws.java
+++ src/test/java/io/github/kurobako/futon/ComonadLaws.java
@@ -1,6 +1,27 @@
+/*
+ * Copyright (C) 2015 Fedor Gavrilov
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
 package io.github.kurobako.futon;
 
-import org.junit.Test;
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
 import javax.annotation.Nonnull;
 
@@ -8,86 +29,85 @@ import static io.github.kurobako.futon.Function.id;
 import static io.github.kurobako.futon.Pair.pair;
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Theories.class)
 public class ComonadLaws {
 
   // extend extract = id
-  @Test
-  public void testStoreExtendExtract() {
-    Store<Character, Integer> one = charAt().extend(Store::extract);
-    Store<Character, Integer> another = charAt();
-    assertEquals(one.extract(), another.extract());
+  @Theory
+  public void testStoreExtendExtract(final Store<Character, Integer> original) {
+    Store<Character, Integer> transformed = original.extend(Store::extract);
+    assertEquals(transformed.extract(), original.extract());
   }
 
   // extract . extend f  = f
-  @Test
-  public void testStoreExtractExtend() {
+  @Theory
+  public void testStoreExtractExtend(final Store<Character, Integer> data) {
     Function<Store<Character, Integer>, Integer> f = store -> store.peeks(i -> i++).hashCode();
-    Integer one = charAt().extend(f).extract();
-    Integer another = f.$(charAt());
+    Integer one = data.extend(f).extract();
+    Integer another = f.$(data);
     assertEquals(one, another);
   }
 
   // extend f . extend g = extend (f . extend g)
-  @Test
-  public void testStoreExtendExtend() {
+  @Theory
+  public void testStoreExtendExtend(final Store<Character, Integer> data) {
     Function<Store<String, Integer>, Character> f = store -> store.extract().charAt(0);
     Function<Store<Character, Integer>, String> g = store -> store.extract().toString();
-    Store<Character, Integer> one = charAt().extend(g).extend(f);
-    Store<Character, Integer> another = charAt().extend(f.of(store -> store.extend(g)));
+    Store<Character, Integer> one = data.extend(g).extend(f);
+    Store<Character, Integer> another = data.extend(f.of(store -> store.extend(g)));
     assertEquals(one.extract(), another.extract());
   }
 
   // extract . duplicate = id
-  @Test
-  public void testStoreExtractDuplicate() {
-    Store<Character, Integer> one = charAt().duplicate().extract();
-    Store<Character, Integer> another = charAt();
-    assertEquals(one.extract(), another.extract());
+  @Theory
+  public void testStoreExtractDuplicate(final Store<Character, Integer> original) {
+    Store<Character, Integer> transformed = original.duplicate().extract();
+    assertEquals(transformed.extract(), original.extract());
   }
 
   // fmap extract . duplicate = id
-  @Test
-  public void testStoreMapExtractDuplicate() {
-    Store<Character, Integer> one = charAt().duplicate().map(Store::extract);
-    Store<Character, Integer> another = charAt();
-    assertEquals(one.extract(), another.extract());
+  @Theory
+  public void testStoreMapExtractDuplicate(final Store<Character, Integer> data) {
+    Store<Character, Integer> transformed = data.duplicate().map(Store::extract);
+    assertEquals(transformed.extract(), data.extract());
   }
 
   // duplicate . duplicate = fmap duplicate . duplicate
-  @Test
-  public void testStoreDuplicateDuplicate() {
-    Store<Store<Store<Character, Integer>, Integer>, Integer> one = charAt().duplicate().duplicate();
-    Store<Store<Store<Character, Integer>, Integer>, Integer> another = charAt().duplicate().map(Store::duplicate);
+  @Theory
+  public void testStoreDuplicateDuplicate(final Store<Character, Integer> data) {
+    Store<Store<Store<Character, Integer>, Integer>, Integer> one = data.duplicate().duplicate();
+    Store<Store<Store<Character, Integer>, Integer>, Integer> another = data.duplicate().map(Store::duplicate);
     assertEquals(one.extract().extract().extract(), another.extract().extract().extract());
   }
 
   // extend f  = fmap f . duplicate
-  @Test
-  public void testStoreExtend() {
+  @Theory
+  public void testStoreExtend(final Store<Character, Integer> data) {
     Function<Store<Character, Integer>, String> f = store -> store.extract().toString();
-    Store<String, Integer> one = charAt().extend(f);
-    Store<String, Integer> another = charAt().duplicate().map(f);
+    Store<String, Integer> one = data.extend(f);
+    Store<String, Integer> another = data.duplicate().map(f);
     assertEquals(one.extract(), another.extract());
   }
 
   // duplicate = extend id
-  @Test
-  public void testStoreDuplicate() {
-    Store<Store<Character, Integer>, Integer> one = charAt().duplicate();
-    Store<Store<Character, Integer>, Integer> another = charAt().extend(id());
+  @Theory
+  public void testStoreDuplicate(final Store<Character, Integer> data) {
+    Store<Store<Character, Integer>, Integer> one = data.duplicate();
+    Store<Store<Character, Integer>, Integer> another = data.extend(id());
     assertEquals(one.extract().extract(), another.extract().extract());
   }
 
   // fmap f = extend (f . extract)
-  @Test
-  public void testStoreMap() {
+  @Theory
+  public void testStoreMap(final Store<Character, Integer> data) {
     Function<Character, String> f = String::valueOf;
-    Store<String, Integer> one = charAt().map(f);
-    Store<String, Integer> another = charAt().extend(f.of(Store::extract));
+    Store<String, Integer> one = data.map(f);
+    Store<String, Integer> another = data.extend(f.of(Store::extract));
     assertEquals(one.extract(), another.extract());
   }
 
-  private static Store<Character, Integer> charAt() {
+  @DataPoint
+  public static Store<Character, Integer> charAt0() {
     return new Store<Character, Integer>() {
       private final String s = "The quick brown fox jumps over the lazy dog";
 
