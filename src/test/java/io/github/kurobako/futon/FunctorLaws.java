b6b4f657ac6329ac510562cf275fad7071cfda18
wyyyrd <wyyyrd@gmail.com>
2015-12-18 15:08:06 +0100

    test case: functor laws for Store

---
+++ src/test/java/io/github/kurobako/futon/FunctorLaws.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2015 Fedor Gavrilov
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+package io.github.kurobako.futon;
+
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import javax.annotation.Nonnull;
+
+import static io.github.kurobako.futon.Function.id;
+import static io.github.kurobako.futon.Pair.pair;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Theories.class)
+public class FunctorLaws {
+
+  // fmap id = id
+  @Theory
+  public void testStoreMapId(final Store<Character, Integer> original) {
+    Store<Character, Integer> transformed = original.map(id());
+    assertEquals(transformed.extract(), original.extract());
+  }
+
+  // fmap (p . q) = (fmap p) . (fmap q)
+  @Theory
+  public void testStoreMapOf(final Store<Character, Integer> data) {
+    Function<Character, String> q = String::valueOf;
+    Function<String, Character> p = s -> s.charAt(0);
+    Store<Character, Integer> one = data.map(p.of(q));
+    Store<Character, Integer> another = data.map(q).map(p);
+    assertEquals(one.extract(), another.extract());
+  }
+
+  @DataPoint
+  public static Store<Character, Integer> charAt0() {
+    return new Store<Character, Integer>() {
+      private final String s = "The quick brown fox jumps over the lazy dog";
+
+      @Override
+      public @Nonnull
+      Pair<Function<? super Integer, ? extends Character>, Integer> run() {
+        return pair(s::charAt, 0);
+      }
+    };
+  }
+}
